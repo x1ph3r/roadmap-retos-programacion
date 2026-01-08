1b9c277042be891cc49bff585f91ea5997b51f30
x1ph3r <ogmoreno4@gmail.com>
2026-01-08 13:39:13 -0500

    x1ph3r.py

---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/x1ph3r.py
@@ -0,0 +1,24 @@
+#tipos de operadores
+"""
+Crea ejemplos utilizando todos los tipos de operadores de tu lenguaje:
+"""
+#1 aritmeticos
+print (5+5)
+print (5**5)
+print (f"Division sin decimales = 27 / 3 = {27 // 3}")
+"""
+(-) RESTA
+(*) MULTIPLIACION 
+(%) RESIDUO
+(**) POTENCIA
+"""
+#2 COMPARACION
+print (f"igualdad: 10 == 3 es { 10 == 3}")
+print (f"difernete : 10 != 3 es { 10 != 3}")
+print (f"mayor : 10 > 3 es { 10 > 3}")
+print (f"menor : 10 < 3 es { 10 < 3}")
+print (f"menor o gual : 10 <= 3 es { 10 <= 3}")
+
+#3 lógicos
+print ("AND &&: 10 + 3 == 13 and 5 - 4 == 1 es {10 + 3 == 13 and 5 - 4 == 1}")
+print ("OR || 10 + 3 == 13 or 5 - 4 == 1 es {10 + 3 == 13 or 5 - 4 == 1}")
